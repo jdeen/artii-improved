62dd6cc2bb38284e8223714174d4605343c55159
Ziyan Junaideen <ziyan@jdeen.com>
2021-12-31 11:28:38 +0530

    Artii Improved GEM

---
+++ lib/artii_improved.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "artii_improved/version"
+
+module ArtiiImproved
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/artii_improved_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ArtiiImproved do
+  it "has a version number" do
+    expect(ArtiiImproved::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
